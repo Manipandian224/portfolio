72aa1fbdfd3fd9165d70823c4e5bb6f6fcf80bce
Manipandian224 <manipandian2006@gmail.com>
2025-05-25 18:43:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const { RiGitBranchFill } = require('react-icons/ri')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderColor : {
+        'primary' :'rgb(85,81,227)',
+        'secondary' : '#2b2d77',
+      }
+    },
+    fontFamily :{
+      'hero-font': "sans-serif",
+    }
+  },
+  plugins: [],
+}
+
